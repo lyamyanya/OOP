274269ea7595fd751526055bfae4e85ed76c9988
lyamyanya <lyamyanya@users.noreply.github.com>
2024-09-03 15:33:48 +0000

    Deploying to gh-pages from @ lyamyanya/OOP@c146e7f18ff44b40687fa57fa5e7349bfc8a8998 🚀

---
+++ Task_1_1_1/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"ru.nsu.bykova","c":"Main","l":"Main()","url":"%3Cinit%3E()"},{"p":"ru.nsu.bykova","c":"Main","l":"main(String[])","url":"main(java.lang.String[])"},{"p":"ru.nsu.bykova","c":"Main","l":"sum(int, int)","url":"sum(int,int)"}]

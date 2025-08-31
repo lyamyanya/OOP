f56dd0b4eb3c9408a19111e3697c175220c0ef90
Galina Bykova <g.bykova@g.nsu.ru>
2025-08-31 21:42:15 +0700

    fix reviewdog

--- Task_1_1_1/src/main/java/ru/nsu/gbykova/sort/Sort.java
+++ Task_1_1_1/src/main/java/ru/nsu/gbykova/sort/Sort.java
@@ -3,10 +3,13 @@ package ru.nsu.gbykova.sort;
 import java.util.Arrays;
 
 /**
- * Class with sort
+ * Class with sort.
  */
 public class Sort {
 
+    /**
+     * Method sort.
+     */
     public static int[] sort(int[] array) {
         var result = array.clone();
         Arrays.sort(result);

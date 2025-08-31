13c780687cfe93676629261917873856b9844704
Galina Bykova <g.bykova@g.nsu.ru>
2025-08-31 21:45:44 +0700

    fix reviewdog

--- Task_1_1_1/src/test/java/ru/nsu/gbykova/sort/SortTest.java
+++ Task_1_1_1/src/test/java/ru/nsu/gbykova/sort/SortTest.java
@@ -7,9 +7,16 @@ import org.junit.jupiter.api.Test;
 class SortTest {
 
     @Test
-    void sort() {
+    void sortSimple() {
         int[] array = new int[]{1, 3, 2};
         var result = Sort.sort(array);
         assertArrayEquals(new int[]{1, 2, 3}, result);
+    }
+
+    @Test
+    void sortEmpty() {
+        int[] array = new int[]{};
+        var result = Sort.sort(array);
+        assertArrayEquals(new int[]{}, result);
     }
 }

ddbfab37db2ecd0dcefc899ef56d0dd5bd85f525
lyamyanya <lyamyanya@users.noreply.github.com>
2025-08-31 14:09:49 +0000

    Deploying to gh-pages from @ lyamyanya/OOP@4370ae01d8b4a9eac99ad5762d227a99156db036 🚀

---
+++ Task_1_1_1/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"ru.nsu.gbykova.Task_1_1_1","c":"Sort","l":"Sort()","u":"%3Cinit%3E()"},{"p":"ru.nsu.gbykova.Task_1_1_1","c":"Sort","l":"sort(int[])"}];updateSearchResults();
